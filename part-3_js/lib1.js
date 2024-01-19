53f8010057aad589a69d82e3e0f7bc59cfdd4b52
Kachhadiya Dharmik <99231778+dharmik2003@users.noreply.github.com>
2024-01-19 13:34:02 +0530

    Delete part-3_js directory

--- part-3_js/lib1.js
+++
@@ -1,1 +0,0 @@
-var libraryname="lib 1"
--- part-3_js/lib2.js
+++
@@ -1,6 +0,0 @@
-// lib1.js already defined global libraryname variable lib2 also create same varible then how to findout varible already exist or not? 
-// solution 
-
-window.libraryname = window.libraryname || "lib 2";  //output: lib 1
-
-// var libraryname="lib 2"
